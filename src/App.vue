3b87af0cae284554d015417fc90cf0115d5a46e1
Adebola <bola@neegles.com>
2024-10-18 09:33:46 +0100

    event card component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
-<script setup></script>
+<script setup>
+import EventCard from '@/components/EventCard.vue'
+</script>
 
 <template>
   <main class="container mx-auto my-8 space-y-8">
     <h1 class="text-4xl font-medium">Event Booking App</h1>
+
     <h2 class="text-2xl font-medium">All Events</h2>
+    <section class="grid grid-cols-2 gap-8">
+      <EventCard
+        v-for="event in 4"
+        :key="event"
+        title="Event Title"
+        event-date="01/01/2023"
+        description="Event description"
+      />
+    </section>
+
     <h2 class="text-2xl font-medium">Your Bookings</h2>
   </main>
 </template>
---
+++ src/components/EventCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="border border-gray-200 bg-white rounded-md">
+    <h3 class="p-4 font-medium text-xl border-b border-gray-200">
+      {{ title }}
+    </h3>
+    <p class="p-4 border-b border-gray-200">{{ eventDate }}</p>
+    <p class="p-4">{{ description }}</p>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  title: String,
+  eventDate: String,
+  description: String,
+})
+</script>
+
+<style lang="scss" scoped></style>
