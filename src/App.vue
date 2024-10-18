b3e0a26fbd3bd8c9da8e8bb47538e6498350c903
Adebola <bola@neegles.com>
2024-10-18 10:52:40 +0100

    fetch events from json server

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,17 @@
     <h1 class="text-4xl font-medium">Event Booking App</h1>
 
     <h2 class="text-2xl font-medium">All Events</h2>
-    <section class="grid grid-cols-2 gap-8">
+    <section class="grid grid-cols-2 gap-8" v-if="!loading">
       <EventCard
-        v-for="event in 4"
+        v-for="event in events"
         :key="event"
-        title="Event Title"
-        event-date="01/01/2023"
-        description="Event description"
+        :title="event.title"
+        :event-date="event.date"
+        :description="event.description"
         @register="handleEventRegistration"
       />
     </section>
+    <section v-else>Loading events...</section>
 
     <h2 class="text-2xl font-medium">Your Bookings</h2>
     <section class="grid grid-cols-1 gap-8">
@@ -27,8 +28,29 @@
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue'
 import EventCard from '@/components/EventCard.vue'
 import BookingItem from '@/components/BookingItem.vue'
+
+const events = ref([])
+const loading = ref(false)
+
+const fetchEvents = async () => {
+  loading.value = true
+  try {
+    const response = await fetch('http://localhost:3001/events')
+    const data = await response.json()
+    events.value = data
+  } catch (error) {
+    console.error(error)
+  } finally {
+    loading.value = false
+  }
+}
+
+onMounted(() => {
+  fetchEvents()
+})
 
 const handleEventRegistration = () => {
   console.log('Register')
