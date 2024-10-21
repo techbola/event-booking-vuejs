8c318e11bf481bc55e911eb4cbc45947cf861f4a
Adebola <bola@neegles.com>
2024-10-21 09:58:03 +0100

    dynamic components for loading icons

--- src/components/BookingItem.vue
+++ src/components/BookingItem.vue
@@ -3,7 +3,9 @@
     <div class="flex justify-between">
       <div class="flex space-x-2">
         <div>{{ title }}</div>
-        <div>{{ status }}</div>
+        <div>
+          <component :is="icon" :class="pending ? 'animate-spin' : ''" />
+        </div>
       </div>
       <RoundButton @click="$emit('cancel')" variant="danger">
         Cancel
@@ -13,13 +15,19 @@
 </template>
 
 <script setup>
+import { computed } from 'vue'
+
 import SectionCard from '@/components/SectionCard.vue'
 import RoundButton from '@/components/RoundButton.vue'
+import { LoaderCircle, Check } from 'lucide-vue-next'
 
-defineProps({
+const props = defineProps({
   title: String,
   status: String,
 })
 
 defineEmits(['cancel'])
+
+const pending = computed(() => props.status === 'pending')
+const icon = computed(() => (pending.value ? LoaderCircle : Check))
 </script>
--- src/components/EventList.vue
+++ src/components/EventList.vue
@@ -1,24 +1,36 @@
 <template>
-  <section class="grid grid-cols-2 gap-8">
-    <template v-if="!loading">
-      <template v-if="events.length">
-        <EventCard
-          v-for="event in events"
-          :key="event.id"
-          :title="event.title"
-          :event-date="event.date"
-          :description="event.description"
-          @register="$emit('register', event)"
-        />
+  <template v-if="error">
+    <SectionCard>
+      <div class="space-y-4 flex flex-col items-center">
+        <div class="text-red-500">{{ error }}</div>
+        <div>
+          <RoundButton @click="fetchEvents"> Retry now </RoundButton>
+        </div>
+      </div>
+    </SectionCard>
+  </template>
+  <template v-else>
+    <section class="grid grid-cols-1 lg:grid-cols-2 gap-8">
+      <template v-if="!loading">
+        <template v-if="events.length">
+          <EventCard
+            v-for="event in events"
+            :key="event.id"
+            :title="event.title"
+            :event-date="event.date"
+            :description="event.description"
+            @register="$emit('register', event)"
+          />
+        </template>
+        <template v-else>
+          <div class="col-span-2 text-center text-gray-500">No events yet.</div>
+        </template>
       </template>
       <template v-else>
-        <div class="col-span-2 text-center text-gray-500">No events yet.</div>
+        <LoadingEventCard v-for="i in 4" :key="i" />
       </template>
-    </template>
-    <template v-else>
-      <LoadingEventCard v-for="i in 4" :key="i" />
-    </template>
-  </section>
+    </section>
+  </template>
 </template>
 
 <script setup>
@@ -26,20 +38,31 @@ import { ref, onMounted } from 'vue'
 
 import EventCard from '@/components/EventCard.vue'
 import LoadingEventCard from '@/components/LoadingEventCard.vue'
+import SectionCard from '@/components/SectionCard.vue'
+import RoundButton from '@/components/RoundButton.vue'
 
 defineEmits(['register'])
 
 const events = ref([])
 const loading = ref(false)
+const error = ref(null)
 
 const fetchEvents = async () => {
   loading.value = true
+  error.value = null
+
   try {
     const response = await fetch('http://localhost:3001/events')
-    const data = await response.json()
-    events.value = data
+
+    if (!response.ok) {
+      error.value = 'Could not fetch events.'
+    } else {
+      const data = await response.json()
+      events.value = data
+    }
   } catch (error) {
     console.error(error)
+    error.value = error
   } finally {
     loading.value = false
   }
