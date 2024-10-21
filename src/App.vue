5973561d2011d94ce82d78608991328dff9fbaeb
Adebola <bola@neegles.com>
2024-10-21 10:44:46 +0100

    BookingList component

--- src/App.vue
+++ src/App.vue
@@ -6,38 +6,13 @@
     <EventList />
 
     <h2 class="text-2xl font-medium">Your Bookings</h2>
-    <section class="grid grid-cols-1 gap-8">
-      <template v-if="!loading">
-        <BookingItem
-          v-for="booking in bookings"
-          :key="booking.id"
-          :title="booking.eventTitle"
-          :status="booking.status"
-          @cancel="handleEventCancellation(booking.id)"
-        />
-      </template>
-      <template v-else>
-        <LoadingBookingItem v-for="i in 4" :key="i" />
-      </template>
-    </section>
+    <BookingList />
   </main>
 </template>
 
 <script setup>
-import { onMounted } from 'vue'
-
 import EventList from '@/components/EventList.vue'
-import BookingItem from '@/components/BookingItem.vue'
-import LoadingBookingItem from '@/components/LoadingBookingItem.vue'
-
-import useBookings from '@/composables/useBookings'
-
-const { bookings, loading, fetchBookings, handleEventCancellation } =
-  useBookings()
-
-onMounted(() => {
-  fetchBookings()
-})
+import BookingList from '@/components/BookingList.vue'
 </script>
 
 <style scoped></style>
---
+++ src/components/BookingList.vue
@@ -0,0 +1,32 @@
+<template>
+  <section class="grid grid-cols-1 gap-8">
+    <template v-if="!loading">
+      <BookingItem
+        v-for="booking in bookings"
+        :key="booking.id"
+        :title="booking.eventTitle"
+        :status="booking.status"
+        @cancel="handleEventCancellation(booking.id)"
+      />
+    </template>
+    <template v-else>
+      <LoadingBookingItem v-for="i in 4" :key="i" />
+    </template>
+  </section>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+
+import BookingItem from '@/components/BookingItem.vue'
+import LoadingBookingItem from '@/components/LoadingBookingItem.vue'
+
+import useBookings from '@/composables/useBookings'
+
+const { bookings, loading, fetchBookings, handleEventCancellation } =
+  useBookings()
+
+onMounted(() => {
+  fetchBookings()
+})
+</script>
